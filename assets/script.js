69803343afef9b50d38eaec814a737c9f643c312
Faris0520 <65797160+Faris0520@users.noreply.github.com>
2021-02-02 00:03:34 +0700

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("links");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+function myFunction2() {
+    var y = document.getElementById("text1");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
